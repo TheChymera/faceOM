2c4b27837fc2a281bf96a12fe645814713c7a62b
Horea Christian <h.chr@mail.ru>
2013-11-15 23:40:26 +0100

    initial commit

---
+++ analysis/MATLAB/first_level_faceOM.m
@@ -0,0 +1,109 @@
+% 1st level batch for Trust Game, Trustee B, from Alena
+
+clear all
+close all
+% List of subjects
+
+
+list_faceOM;
+
+
+% loop over subjects in list
+for s=1:length(list)
+    
+    subject = list{s};
+    newFolder = ['/home/klips/Documents/Eye_Tracking/first_level/' subject '/results/faceOM/'];
+ 
+    
+    for i=1:520 
+        array_scan{i} =  ['/home/klips/Documents/Eye_Tracking/first_level/' subject '/epi/faceOM/swa' subject '_ep2d_faceOM.nii,' num2str(i)]; 
+    end
+    %define onset file
+    onset_file = ['/home/klips/Documents/Eye_Tracking/first_level/' subject '/onsets/' subject '-faceOM_onset.mat'];
+    %define movement regressor file
+    movement_file = ['/home/klips/Documents/Eye_Tracking/first_level/' subject '/epi/faceOM/rp_a' subject '_ep2d_faceOM.txt'];
+    %starts spm
+    spm('defaults', 'FMRI');
+
+    %-----------------------SPM Batch Job----------------------------------
+    
+ %-----------------------------------------------------------------------
+% Job configuration created by cfg_util (rev $Rev: 3944 $)
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.fmri_spec.dir = {newFolder};
+matlabbatch{1}.spm.stats.fmri_spec.timing.units = 'scans';
+matlabbatch{1}.spm.stats.fmri_spec.timing.RT = 2;
+matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t = 33;
+matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t0 = 17;
+%%
+matlabbatch{1}.spm.stats.fmri_spec.sess.scans = array_scan;
+%%
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {});
+matlabbatch{1}.spm.stats.fmri_spec.sess.multi = {onset_file};
+matlabbatch{1}.spm.stats.fmri_spec.sess.regress = struct('name', {}, 'val', {});
+matlabbatch{1}.spm.stats.fmri_spec.sess.multi_reg = {movement_file};
+matlabbatch{1}.spm.stats.fmri_spec.sess.hpf = 128;
+matlabbatch{1}.spm.stats.fmri_spec.fact = struct('name', {}, 'levels', {});
+matlabbatch{1}.spm.stats.fmri_spec.bases.hrf.derivs = [1 1];
+matlabbatch{1}.spm.stats.fmri_spec.volt = 1;
+matlabbatch{1}.spm.stats.fmri_spec.global = 'None';
+matlabbatch{1}.spm.stats.fmri_spec.mask = {''};
+matlabbatch{1}.spm.stats.fmri_spec.cvi = 'AR(1)';
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep;
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1).tname = 'Select SPM.mat';
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1).tgt_spec{1}(1).name = 'filter';
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1).tgt_spec{1}(1).value = 'mat';
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1).tgt_spec{1}(2).name = 'strtype';
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1).tgt_spec{1}(2).value = 'e';
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1).sname = 'fMRI model specification: SPM.mat File';
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1).src_exbranch = substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1});
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1).src_output = substruct('.','spmmat');
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep;
+matlabbatch{3}.spm.stats.con.spmmat(1).tname = 'Select SPM.mat';
+matlabbatch{3}.spm.stats.con.spmmat(1).tgt_spec{1}(1).name = 'filter';
+matlabbatch{3}.spm.stats.con.spmmat(1).tgt_spec{1}(1).value = 'mat';
+matlabbatch{3}.spm.stats.con.spmmat(1).tgt_spec{1}(2).name = 'strtype';
+matlabbatch{3}.spm.stats.con.spmmat(1).tgt_spec{1}(2).value = 'e';
+matlabbatch{3}.spm.stats.con.spmmat(1).sname = 'fMRI model specification: SPM.mat File';
+matlabbatch{3}.spm.stats.con.spmmat(1).src_exbranch = substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1});
+matlabbatch{3}.spm.stats.con.spmmat(1).src_output = substruct('.','spmmat');
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'em100_HA > cell22';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.convec = [1/3 1/3 1/3 0 0 0 0 0 0 0 0 0 -1/3 -1/3 -1/3];
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.name = 'em100_FE > cell22';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.convec = [0 0 0  1/3 1/3 1/3 0 0 0 0 0 0 -1/3 -1/3 -1/3];
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{3}.tcon.name = 'em40_HA > cell10';
+matlabbatch{3}.spm.stats.con.consess{3}.tcon.convec = [0 0 0 0 0 0  1/3 1/3 1/3 0 0 0 0 0 0 -1/3 -1/3 -1/3];
+matlabbatch{3}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{4}.tcon.name = 'em40_FE > cell10';
+matlabbatch{3}.spm.stats.con.consess{4}.tcon.convec = [0 0 0 0 0 0 0 0 0 1/3 1/3 1/3  0 0 0 -1/3 -1/3 -1/3];
+matlabbatch{3}.spm.stats.con.consess{4}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{5}.tcon.name = 'cell22 > cell10';
+matlabbatch{3}.spm.stats.con.consess{5}.tcon.convec = [0 0 0 0 0 0 0 0 0 0 0 0  1/3 1/3 1/3  -1/3 -1/3 -1/3];
+matlabbatch{3}.spm.stats.con.consess{5}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'em100 > cell22';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.convec = [1/3 1/3 1/3 0 0 0 0 0 0 0 0 0 -1/3 -1/3 -1/3];
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'em40 > cell10';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.convec = [1/3 1/3 1/3 0 0 0 0 0 0 0 0 0 -1/3 -1/3 -1/3];
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'em100 > em40';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.convec = [1/3 1/3 1/3 0 0 0 0 0 0 0 0 0 -1/3 -1/3 -1/3];
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{6}.fcon.name = 'alles';
+matlabbatch{3}.spm.stats.con.consess{6}.fcon.convec = {
+                                                       eye(24)
+                                                       }';
+matlabbatch{3}.spm.stats.con.consess{6}.fcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.delete = 0;
+
+
+    %-----------------------SPM Batch Job END------------------------------
+    
+    %runs matlab batch
+    spm_jobman('run', matlabbatch)
+
+
+end
---
+++ analysis/data_functions.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+__author__ = 'Horea Christian'
+
+
+def get_and_filter_results(experiment=False, source=False, prepixelation='not specified', remove='', mismeasurement='remove', apply_correct_values=False, make_COI=False):
+	import pandas as pd
+	from os import path
+	import sys
+	from chr_helpers import get_config_file
+
+	config = get_config_file(localpath=path.dirname(path.realpath(__file__))+'/')
+	
+	if isinstance(prepixelation, (int, long)):
+		print prepixelation
+	
+	#IMPORT VARIABLES
+	if prepixelation == 'not specified': #is not triggers if the value is defined as 0 .
+		prepixelation = config.getint('Data', 'prepixelation')
+	if not experiment:
+		experiment = config.get('Data', 'experiment')
+	if not source:
+		source = config.get('Source', 'source')
+	data_path = config.get('Addresses', source)
+	ignore_filename = config.get('Data', 'ignore_filename')
+	#END IMPORT VARIABLES
+	
+	if source == 'server':
+		from HTMLParser import HTMLParser
+		import urllib
+		class ChrParser(HTMLParser):
+			def handle_starttag(self, tag, attrs):
+				if tag =='a':
+					for key, value in attrs:
+						if key == 'href' and value.endswith('.csv'):
+							pre_fileslist.append(value)
+		results_dir = data_path+experiment+'/px'+str(prepixelation)+'/'
+		print results_dir
+		data_url = urllib.urlopen(results_dir).read()
+		parser = ChrParser()
+		pre_fileslist = []
+		parser.feed(data_url) # pre_fileslist gets populated here
+	elif source == 'live':
+		from os import listdir
+		results_dir = path.dirname(path.dirname(path.realpath(__file__))) + data_path + str(prepixelation) + '/'
+		results_dir = path.expanduser(results_dir)
+		pre_fileslist = listdir(results_dir)
+	elif source == 'local':
+		from os import listdir
+		results_dir = data_path + experiment + '/px' + str(prepixelation) + '/'
+		results_dir = path.expanduser(results_dir)
+		pre_fileslist = listdir(results_dir)
+		
+	print('Loading data from '+results_dir)
+		
+	if pre_fileslist == []:
+		raise InputError('For some reason the list of results files could not be populated.')
+	files = [lefile for lefile in pre_fileslist if lefile.endswith('.csv') and not lefile.endswith(ignore_filename+'.csv')]
+	
+	data_all = pd.DataFrame([]) # empty container frame for concatenating input from multiple files
+	for lefile in files:
+		data_lefile = pd.DataFrame.from_csv(results_dir+lefile)
+		data_lefile['ID'] = path.splitext(lefile)[0]
+		scrambling_list = set(data_lefile['scrambling'])
+		if apply_correct_values:
+			data_lefile=correct_values(data_lefile)	
+		if make_COI:
+			data_lefile = categories_of_interest(data_lefile, scrambling_list)
+		elif mismeasurement == 'fix':
+			make_COI == True
+			data_lefile = categories_of_interest(data_lefile, scrambling_list)
+		if mismeasurement == 'remove':
+			data_lefile = data_lefile[data_lefile['RT'] >0] # remove entries with instant RTs here
+		elif mismeasurement == 'nan':
+			data_lefile.ix[(data_lefile['RT'] <=0), 'RT'] = False # remove entries with incorrect answers here
+		elif mismeasurement == 'fix':
+			import numpy as np
+			for COI in set(data_lefile['COI']):
+				data_lefile.ix[(data_lefile['RT'] <=0) & (data_lefile['COI'] == COI), 'RT'] = np.median(data_lefile[data_lefile['COI'] == COI]['RT']) #replace missing values with the median of the repecitive COI
+		if 'no-response' in remove:
+			data_lefile = data_lefile[data_lefile['keypress'] != 'none'] # remove entries with no answers here
+		if 'incorrect' in remove:
+			data_lefile = data_lefile[data_lefile['correct answer'] == data_lefile['keypress']] # remove entries with incorrect answers here
+		data_all = pd.concat([data_all, data_lefile], ignore_index=True)
+	return data_all
+	
+def categories_of_interest(data_frame, scrambling_list):
+	# DEFINE CATEGORIES OF INTEREST (COI)
+	data_frame['COI']='' 
+	data_frame.ix[(data_frame['scrambling'] == 0) & (data_frame['intensity'] == 100), 'COI'] = 'em-easy'
+	data_frame.ix[(data_frame['scrambling'] == 0) & (data_frame['intensity'] == 40), 'COI'] = 'em-hard'
+	for i in scrambling_list:
+		if i != 0: #don't overwrite emotion tags
+			data_frame.ix[(data_frame['scrambling'] == i), 'COI'] = 'sc-' + str(i)
+			data_frame.ix[(data_frame['scrambling'] == i), 'COI'] = 'sc-' + str(i)
+			data_frame.ix[(data_frame['scrambling'] == i), 'COI'] = 'sc-' + str(i)
+			data_frame.ix[(data_frame['scrambling'] == i), 'COI'] = 'sc-' + str(i)
+			data_frame.ix[(data_frame['scrambling'] == i), 'COI'] = 'sc-' + str(i)
+	# END DEFINE CATEGORIES OF INTEREST (COI)
+	return data_frame
+
+def correct_values(data):
+	data.ix[(data['scrambling'] != 0), 'intensity'] = 100
+	return data
+	
+if __name__ == '__main__':
+	main()
+	show()
