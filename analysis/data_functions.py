525a9fed2bde6e8cc62e8f5189f08c0e181792f7
Horea Christian <h.chr@mail.ru>
2013-11-21 07:13:04 +0100

    added time course functionality

--- analysis/data_functions.py
+++ analysis/data_functions.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python
 __author__ = 'Horea Christian'
 
-
-cats = [
-	['difficulty', 'easy', 'em100|cell22rand'],
-	['difficulty', 'hard', 'em40|cell10rand'],
-	['emotion', 'happy', 'HA-cell'],
-	['emotion', 'fearful', 'FE-cell'],
-	['emotion', 'scrambled', 'cell']
-	]
-
-def get_et_data(source=False, make='timecourse', make_categories=cats):
+def get_et_data(source=False, make='timecourse', make_categories='', savefile='', force_new=False):
 	from os import path
 	import sys
 	import pandas as pd
@@ -24,21 +15,31 @@ def get_et_data(source=False, make='timecourse', make_categories=cats):
 	if not source:
 		source = config.get('Source', 'source')
 	data_path = config.get('Addresses', source)
+	preprocessed_path = config.get('Data', 'df_dir')
 	#END IMPORT VARIABLES
+	
 	
 	if source == 'local':
 		from os import listdir
 		data_path = path.expanduser(data_path)
 		pre_fileslist = listdir(data_path)
+		if savefile:
+			preprocessed_file = path.expanduser(data_path+preprocessed_path+savefile)
+			if path.exists(preprocessed_file) and not force_new:
+				data_all = pd.DataFrame.from_csv(preprocessed_file)
+				data_all = data_all.set_index(['CoI'],append=True, drop=True)
+				data_all = data_all.set_index(['measurement'],append=True, drop=True)
+				data_all = data_all.reorder_levels(['ID','CoI','measurement'])
+				return data_all
 		
 	print('Loading data from '+data_path)
 	if pre_fileslist == []:
 		raise InputError('For some reason the list of results files could not be populated.')
 
 	files = [lefile for lefile in pre_fileslist if lefile.endswith('.txt')]
-	files = files[3:4]
+	#~ files = files[3:4]
 	
-	data_all = pd.DataFrame([]) # empty container frame for concatenating input from multiple files
+	data_all = [] # empty container list for listing per-file dataframes
 	for lefile in files:
 		print lefile
 		data_lefile = pd.DataFrame.from_csv(data_path+lefile, header=42, sep='\t')
@@ -57,15 +58,14 @@ def get_et_data(source=False, make='timecourse', make_categories=cats):
 		#END CUTOFF AT 'pulse_start'
 		
 		if make_categories:
-			new_category_names = set([new_cat[0] for new_cat in cats])
+			new_category_names = set([new_cat[0] for new_cat in make_categories])
 			for new_category_name in new_category_names:
 				data_lefile[new_category_name]='' 
 			trial_key = data_lefile[(data_lefile['Type'] == 'MSG')][['Trial','L Raw X [px]']].rename(columns={'L Raw X [px]': 'message'}) #crop and rename
 			trial_key = np.array(trial_key) #for easier iteration
-			for category in cats:
+			for category in make_categories:
 				criterion = category[-1]
 				for trial in trial_key:
-					trial[1] = trial[1].split(' ')[-1]
 					if '-' in criterion:
 						if criterion.split('-')[0] in trial[1] and criterion.split('-')[1] not in trial[1]:
 							data_lefile.ix[(data_lefile['Trial']==trial[0]), category[0]] = category[1]
@@ -81,38 +81,49 @@ def get_et_data(source=False, make='timecourse', make_categories=cats):
 		data_lefile['measurement'] = grouped.apply(lambda x: pd.Series(np.arange(len(x)), x.index))
 		data_lefile.set_index(['Trial', 'measurement'], inplace=True)
 		#~ #MAKE MULTIINDEX
+		#~ print data_lefile.xs(320, level=1)
+		#~ print data_lefile.columns.tolist()
 		
 		if make == 'timecourse':
-			data_all = pd.DataFrame([])
-			for category in cats:
+			groups_all = []
+			for category in make_categories:
 				group = data_lefile[(data_lefile[category[0]] == category[1])]
 				group = group.groupby(level=1).mean()
-				group['Time'] = group['Time']-group['Time'].min()
+				group = group.ix[:240]
+				group['Time'] = group['Time']-group['Time'].ix[0]
 				group['CoI'] = ''
 				group['CoI'] = category[1]
 				group = group.set_index(['CoI'], append=True, drop=True)
 				group = group.reorder_levels(['CoI','measurement'])
-				data_all = pd.concat([data_all, group], ignore_index=False)
-				#~ data_all.set_index(['CoI', 'mesurement'])
-				print np.shape(data_all), data_all.ix[1]
+				groups_all.append(group)
+			data_lefile = pd.concat(groups_all)
 		elif isinstance(make, int):
-			data_lefile.set_index(['measurement'], inplace=True)
-			print data_lefile.unstack().columns.tolist()
+			data_lefile = data_lefile.reset_index()
+			#~ print data_lefile.columns.tolist()
 			avg = data_lefile.groupby("Trial")['L Dia X [px]'].agg({0: lambda x: x.head((len(x)+1)//make).mean(), 
                                        1: lambda x: x.tail((len(x)+1)//make).mean()}) 
-			result = pd.melt(avg.reset_index(), "Trial", var_name="Measurement", value_name="L Dia X [px]")
-			result = result.sort("Trial").set_index(["Trial", "Measurement"])
+			result = pd.melt(avg.reset_index(), "Trial", var_name="measurement", value_name="L Dia X [px]")
+			result = result.sort("Trial").set_index(["Trial", "measurement"])
 			print result
 		else:
 			print 'Please specify the "make" argumant as either "timecourse" or an integer.'
 	
 		#ADD ID
-		#~ data_lefile['ID']=lefile.split('_')[0]
-		#~ data_lefile = data_lefile.set_index(['ID'], append=True, drop=True)
-		#~ data_lefile = data_lefile.reorder_levels(['ID','Trial','measurement'])
+		data_lefile['ID']=lefile.split('_')[0]
+		data_lefile = data_lefile.set_index(['ID'], append=True, drop=True)
+		data_lefile = data_lefile.reorder_levels(['ID','CoI','measurement'])
 		#END ADD ID
+		
+		data_all.append(data_lefile)
+	data_all = pd.concat(data_all)
+	data_all.reorder_levels(['ID','CoI','measurement'])
+	
+	if savefile:
+		data_all.to_csv(preprocessed_file)
+	
+	return data_all
 
-def categories_of_interest(data_frame, scrambling_list):
+def make_categories_of_interest(data_frame, scrambling_list):
 	# DEFINE CATEGORIES OF INTEREST (COI)
 	data_frame['COI']='' 
 	data_frame.ix[(data_frame['scrambling'] == 0) & (data_frame['intensity'] == 100), 'COI'] = 'em-easy'
@@ -128,5 +139,5 @@ def categories_of_interest(data_frame, scrambling_list):
 	return data_frame
 
 if __name__ == '__main__':
-	get_et_data(make=2)
+	get_et_data()
 	#~ show()
--- analysis/et.py
+++ analysis/et.py
@@ -1,6 +1,54 @@
 #!/usr/bin/env python
 from __future__ import division
 __author__ = 'Horea Christian'
+from data_functions import get_et_data
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+from pylab import figure, show, errorbar, setp, legend
 
-def main(experiment=False, source=False, prepixelation='not specified', elinewidth=2, ecolor='0.3', make_tight=True, total='means', make_std=False, make_sem=True):
-    data_all = get_and_filter_results(experiment, source, prepixelation, mismeasurement='fix', apply_correct_values=True, make_COI=True)
+categories = [
+	['difficulty', 'easy', 'em100|cell22rand'],
+	['difficulty', 'hard', 'em40|cell10rand'],
+	['emotion', 'happy', 'HA-cell'],
+	['emotion', 'fearful', 'FE-cell'],
+	['emotion', 'scrambled', 'cell'],
+    ['fixation', 'fix', 'fix']
+	]
+
+def main(source=False, make_tight=True):
+    timecourse = get_et_data(make='timecourse', make_categories=categories, savefile='time_series.csv', force_new=True)
+    #~ print timecourse.index.tolist()
+
+
+    timecourse_means = timecourse.groupby(level=(1,2)).mean()
+    
+    timecourse_fix_mean = (timecourse_means.ix["fix"]["L Dia Y [px]"].mean()+timecourse_means.ix["fix"]["L Dia Y [px]"].mean())/2
+    #~ normalize = lambda x: (x / timecourse_fix_mean)
+    
+    #~ timecourse_means = timecourse_means.groupby(level=0).transform(normalize)
+    #~ timecourse_means = timecourse.groupby(level=(1,2)).aggregate(np.median)
+    timecourse_sems = timecourse.groupby(level=(1,2)).aggregate(stats.sem)
+    
+    #~ print timecourse_sems.ix['easy'].ix[:10] 
+    
+    fig = figure(figsize=(4, 3), dpi=300,facecolor='#eeeeee', tight_layout=make_tight)
+    ax1=fig.add_subplot(2,2,1)
+    ax2=fig.add_subplot(2,2,2)
+    ax3=fig.add_subplot(2,2,3)
+    ax4=fig.add_subplot(2,2,4)
+    ax1.yaxis.grid(True, linestyle='-', which='major', color='#dddddd',alpha=0.6, zorder = 0)
+    ax1.set_axisbelow(True)
+    
+    #~ plt.plot(np.array(timecourse_means.ix["easy"]["Time"])/1000, np.array(timecourse_means.ix["easy"]["L Dia X [px]"]), color='m')
+    #~ plt.plot(np.array(timecourse_means.ix["easy"]["Time"])/1000, np.array(timecourse_means.ix["easy"]["L Dia Y [px]"]), color='g')
+    ax1.plot(np.array(timecourse_means.ix["easy"]["Time"])/1000, (np.array(timecourse_means.ix["easy"]["L Dia Y [px]"])+np.array(timecourse_means.ix["easy"]["L Dia X [px]"]))/timecourse_fix_mean/2, color='g')
+    ax1.plot(np.array(timecourse_means.ix["hard"]["Time"])/1000, (np.array(timecourse_means.ix["hard"]["L Dia Y [px]"])+np.array(timecourse_means.ix["hard"]["L Dia X [px]"]))/timecourse_fix_mean/2, color='m')
+    ax2.plot(np.array(timecourse_means.ix["happy"]["Time"])/1000, (np.array(timecourse_means.ix["happy"]["L Dia Y [px]"])+np.array(timecourse_means.ix["happy"]["L Dia X [px]"]))/timecourse_fix_mean/2, color='g')
+    ax2.plot(np.array(timecourse_means.ix["fearful"]["Time"])/1000, (np.array(timecourse_means.ix["fearful"]["L Dia Y [px]"])+np.array(timecourse_means.ix["fearful"]["L Dia X [px]"]))/timecourse_fix_mean/2, color='m')
+    ax3.plot(np.array(timecourse_means.ix["scrambled"]["Time"])/1000, (np.array(timecourse_means.ix["scrambled"]["L Dia Y [px]"])+np.array(timecourse_means.ix["scrambled"]["L Dia X [px]"]))/timecourse_fix_mean/2, color='m')
+    ax3.plot((np.array(timecourse_means.ix["happy"]["Time"])+np.array(timecourse_means.ix["happy"]["Time"]))/2000, (np.array(timecourse_means.ix["happy"]["L Dia Y [px]"])+np.array(timecourse_means.ix["happy"]["L Dia X [px]"])+np.array(timecourse_means.ix["fearful"]["L Dia Y [px]"])+np.array(timecourse_means.ix["fearful"]["L Dia X [px]"]))/timecourse_fix_mean/4, color='g')
+
+if __name__ == '__main__':
+    main()
+    show()
